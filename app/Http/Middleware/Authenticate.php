9f72df35d605975ad52aee36b7f5fc75bc79f115
Rafael André Hoffmann <rhoffmann@nelogica.com.br>
2021-02-23 22:31:23 -0300

    Pré-cadastro ADM

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,6 +14,9 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+        if(! $request->expectsJson() && $request->is('admin*')) {
+            return route('admin.login');
+        }
         if (! $request->expectsJson()) {
             return route('login');
         }
