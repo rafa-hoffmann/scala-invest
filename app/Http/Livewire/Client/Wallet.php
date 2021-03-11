1e68a5f968d9bd48063ef7a9315395025205f8fc
Rafael André Hoffmann <rhoffmann@nelogica.com.br>
2021-03-11 20:30:39 -0300

    Início do layout das carteiras

---
+++ app/Http/Livewire/Client/Wallet.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Client;
+
+use Livewire\Component;
+
+class Wallet extends Component
+{
+    public function render()
+    {
+        return view('client.livewire.wallet');
+    }
+}
---
+++ resources/views/client/livewire/wallet.blade.php
@@ -0,0 +1,53 @@
+<div>
+    <div class="flex flex-wrap w-full justify-end">
+        <div class="px-2 w-2/12 overflow-hidden">
+            <div class="flex flex-col space-y-2">
+                <button class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-10 rounded-full">
+                    Nova Carteira
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-wrap overflow-hidden items-center shadow-md">
+        <div class="my-2 px-2 w-10/12 overflow-hidden">
+            <div id="profile" class="shadow-2xl bg-white opacity-80">
+                <div class="text-center lg:text-left">
+                    <h1 class="text-3xl font-bold pt-8 lg:pt-0">Carteira 1</h1>
+                    <div class="pt-3 border-b-2 border-green-500 opacity-50"></div>
+                    <p class="pt-4 pb-4 text-base font-bold flex items-center justify-center lg:justify-start"><svg
+                            class="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20">
+                            <path
+                                d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" />
+                        </svg> Indústria Agrícola</p>
+                    <div class="flex space-x-2">
+                        <p>PETR4</p>
+                        <p>BRFS3</p>
+                        <p>VALE3</p>
+                    </div>
+                    <p class="pt-4 pb-4 text-base font-bold flex items-center justify-center lg:justify-start"><svg
+                            class="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20">
+                            <path
+                                d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" />
+                        </svg> Indústria Agrícola</p>
+                    <div class="flex space-x-2">
+                        <p>PETR4</p>
+                        <p>BRFS3</p>
+                        <p>VALE3</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="my-2 px-2 w-2/12 overflow-hidden">
+            <div class="flex flex-col space-y-2 pt-8 pb-8">
+                <button class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full">
+                    Atualizar
+                </button>
+                <button class="bg-red-700 hover:bg-red-900 text-white font-bold py-2 px-4 rounded-full">
+                    Excluir
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
