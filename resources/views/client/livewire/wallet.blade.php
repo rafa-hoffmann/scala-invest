960e6caf0d8241bac5d5472db1d667988fd4b995
Rafael André Hoffmann <rhoffmann@nelogica.com.br>
2021-03-30 09:32:53 -0300

    Mais correções no layout

--- resources/views/client/livewire/wallet.blade.php
+++ resources/views/client/livewire/wallet.blade.php
@@ -1,8 +1,8 @@
 <div>
-    <div class="flex flex-wrap w-full justify-end mt-5">
-        <div class="px-2 w-2/12 overflow-hidden">
-            <div class="flex flex-col space-y-2">
-                <a class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-10 rounded-full"
+    <div class="flex flex-wrap w-full mt-5">
+        <div class="w-full px-4 lg:w-2/12 overflow-hidden">
+            <div class="flex flex-col space-y-2 px-4">
+                <a class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full text-center"
                     href="{{route('wallet.update')}}">
                     Nova Carteira
                 </a>
@@ -10,15 +10,15 @@
         </div>
     </div>
     @foreach($wallets as $wallet)
-    <div class="flex flex-wrap overflow-hidden items-center shadow-md">
-        <div class="my-2 w-10/12 overflow-hidden">
+    <div class="flex flex-wrap overflow-hidden items-center my-8 lg:-mx-4">
+        <div class="w-full overflow-hidden lg:my-4 lg:px-4 lg:w-10/12">
             <div class="text-center lg:text-left">
                 <h1 class="text-3xl font-bold pt-8 lg:pt-0 ml-2">{{empty($wallet->name) ? 'Carteira' : $wallet->name}}
                 </h1>
                 <div class="pt-3 border-b-2 border-green-500 opacity-50"></div>
                 <div class="flex flex-col">
                     <div class="py-2 align-middle inline-block min-w-full">
-                        <table class="min-w-full divide-y divide-gray-200">
+                        <table class="min-w-full divide-y divide-gray-200 m-2 shadow-md rounded-md">
                             <thead class="bg-gray-50">
                                 <tr>
                                     <th scope="col"
@@ -51,7 +51,7 @@
                                     </th>
                                     <th scope="col"
                                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Distância do Objetivo
+                                        Dist. Do Objetivo
                                     </th>
                                 </tr>
                             </thead>
@@ -75,14 +75,13 @@
                                         R$ {{number_format($stock->patrimonio_att, 2, ',', '.')}}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
-                                        {{($wallet->soma_patrimonio == 0)?0.00:number_format($stock->patrimonio_att / $wallet->soma_patrimonio * 100 , 2, ',', '.')}}
-                                        %
+                                        {{($wallet->soma_patrimonio == 0)?0.00:number_format($stock->patrimonio_att / $wallet->soma_patrimonio * 100 , 2, ',', '.')}}%
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         {{number_format($stock->pivot->goal, 2, ',', '.')}}%
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
-                                        {{number_format($stock->pivot->goal_distance,2,',','.')}} %
+                                        {{number_format($stock->pivot->goal_distance,2,',','.')}}%
                                     </td>
                                 </tr>
                                 @endforeach
@@ -92,8 +91,8 @@
                 </div>
             </div>
         </div>
-        <div class="my-2 px-2 w-2/12 overflow-hidden">
-            <div class="flex flex-col space-y-2 pt-8 pb-8">
+        <div class="w-full overflow-hidden lg:my-4 lg:px-4 lg:w-2/12">
+            <div class="flex flex-col space-y-2 py-8 px-4">
                 <a class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full text-center"
                     href="{{route('wallet.update', ['id' => $wallet->id])}}">
                     Atualizar
@@ -104,29 +103,30 @@
                 </a>
             </div>
         </div>
-        <div class="flex flex-wrap w-full overflow-hidden lg:-mx-2">
-            <div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/2">
-                <div class="block h-80 pb-5">
-                    <h1 class="text-xl font-bold pt-8 lg:pt-0 ml-2">Atual</h1>
-                    <div id="currentChart{{$wallet->id}}" class="max-h-80"></div>
-                </div>
-            </div>
-            <div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/2">
-                <div class="block h-80 pb-5">
-                    <h1 class="text-xl font-bold pt-8 lg:pt-0 ml-2">Recomendado</h1>
-                    <div id="recommendedChart{{$wallet->id}}" class="max-h-80"></div>
-                </div>
-            </div>
-
-        </div>
-        <div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-full">
+    </div>
+    <div class="flex flex-wrap w-full overflow-hidden lg:-mx-2">
+        <div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/2">
             <div class="block h-80 pb-5">
-                <h1 class="text-xl font-bold pt-8 lg:pt-0 ml-2">Setores</h1>
-                <div id="sectorChart{{$wallet->id}}" class="max-h-80"></div>
+                <h1 class="text-xl font-bold pt-8 lg:pt-0 ml-2">Atual</h1>
+                <div id="currentChart{{$wallet->id}}" class="max-h-80"></div>
             </div>
         </div>
-        <script>
-            var optionsCurrentChart = {
+        <div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/2">
+            <div class="block h-80 pb-5">
+                <h1 class="text-xl font-bold pt-8 lg:pt-0 ml-2">Recomendado</h1>
+                <div id="recommendedChart{{$wallet->id}}" class="max-h-80"></div>
+            </div>
+        </div>
+
+    </div>
+    <div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-full">
+        <div class="block h-80 pb-5">
+            <h1 class="text-xl font-bold pt-8 lg:pt-0 ml-2">Setores</h1>
+            <div id="sectorChart{{$wallet->id}}" class="max-h-80"></div>
+        </div>
+    </div>
+    <script>
+        var optionsCurrentChart = {
                     series: @json($stockChart[$wallet->id]['data']),
                     chart: {
                         type: 'donut',
@@ -194,7 +194,7 @@
                 };
 
             new ApexCharts(document.querySelector("#sectorChart{{$wallet->id}}"), optionsSectorChart).render();
-        </script>
-    </div>
-    @endforeach
+    </script>
+</div>
+@endforeach
 </div>
